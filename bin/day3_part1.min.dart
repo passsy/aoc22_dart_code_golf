bd0338b1ca1e8a2803936a84b9dbf477ddc1486e
Pascal Welsch <pascal@welsch.dev>
2022-12-03 19:09:37 +0000

    Remove var

--- bin/day3_part1.min.dart
+++ bin/day3_part1.min.dart
@@ -1,1 +1,1 @@
-main(List<String>a,{h,i,s=0}){for(i in a){var v=i.runes.map((c)=>c>96?c-96:c-38);h=v.length~/2;s+=v.take(h).where((c)=>v.skip(h).contains(c)as bool).first;};print(s);}
+main(List<String>a,{h,i,s=0,v}){for(i in a){v=i.runes.map((c)=>c>96?c-96:c-38);h=v.length~/2;s+=v.take(h).where((c)=>v.skip(h).contains(c)as bool).first;};print(s);}
