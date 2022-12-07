60b90e5dde3440c692934de0f0b4307f8ff8f22b
Pascal Welsch <pascal@welsch.dev>
2022-12-07 10:34:21 +0000

    Solve day 7 part 1

---
+++ bin/day7_part1.dart
@@ -0,0 +1,56 @@
+void main(List<String> args) {
+  Node cwd = root;
+  final tree = [];
+  for (String c in args) {
+    if (c.startsWith('\$ cd')) {
+      if (c.endsWith('..')) {
+        cwd = cwd.parent!;
+      } else {
+        final old = cwd;
+        cwd = Node.dir('${cwd.name}/${c.substring(5)}', old);
+        old.children.add(cwd);
+        tree.add(cwd);
+      }
+      continue;
+    }
+    if (c.startsWith('\$ ls')) {
+      // ignore
+      continue;
+    }
+    if (c.startsWith('dir')) {
+      // subfolder = c.substring(4);
+      continue;
+    }
+
+    // fileSize name
+    final parts = c.split(' ');
+    cwd.children.add(Node.file(parts[1], int.parse(parts[0])));
+    // print('Unhandled: $c');
+  }
+
+  final size = tree.where((a) => a.size < 100000).map((a) => a.size).reduce((a, b) => a + b);
+
+  print(size);
+}
+
+class Node {
+  final String name;
+  final List<Node> children = [];
+  final int? fileSize;
+  final Node? parent;
+
+  Node.file(this.name, this.fileSize) : parent = null;
+
+  Node.dir(String name, this.parent)
+      :name= name.replaceAll('///', '/').replaceAll('//', '/'),
+        fileSize = null;
+
+  int get size => fileSize ?? children.fold(0, (a, b) => a + b.size);
+
+  @override
+  String toString() {
+    return 'Node{name: $name}';
+  }
+}
+
+final root = Node.dir('/',null);
---
+++ test/day7_test.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+
+import 'package:test/test.dart';
+import '../bin/day7_part1.dart' as day7_part1;
+import '../bin/day7_part1.min.dart' as day7_part1_min;
+import '../bin/day7_part2.dart' as day7_part2;
+import '../bin/day7_part2.min.dart' as day7_part2_min;
+import 'main_tester.dart';
+
+void main() {
+  group('day 7', () {
+    test('sample part 1', () {
+      final output = testMain(
+        day7_part1.main,
+        input: File('data/day7_sample.txt').readAsStringSync(),
+      );
+      expect(output, '95437');
+    });
+
+    test('golf part 1', () {
+      final output = testMain(
+        day7_part1_min.main,
+        input: File('data/day7_sample.txt').readAsStringSync(),
+      );
+      expect(output, '95437');
+    });
+
+    test('solve part 1', () {
+      final output = testMain(
+        day7_part1.main,
+        input: File('data/day7_input.txt').readAsStringSync(),
+      );
+      print(output);
+    });
+
+    test('sample part 2', () {
+      final output = testMain(
+        day7_part2.main,
+        input: File('data/day7_sample.txt').readAsStringSync(),
+      );
+      expect(output, '100');
+    });
+
+    test('golf part 2', () {
+      final output = testMain(
+        day7_part2_min.main,
+        input: File('data/day7_sample.txt').readAsStringSync(),
+      );
+      expect(output, '100');
+    });
+
+    test('solve part 2', () {
+      final output = testMain(
+        day7_part2.main,
+        input: File('data/day7_input.txt').readAsStringSync(),
+      );
+      print(output);
+    });
+  });
+}
